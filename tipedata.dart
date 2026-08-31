b44db84a0489740d080c43cb42d3fd1d52659dc2
Nielskibidi <ngnielniel@gmail.com>
2026-08-31 20:26:37 +0700

    update 31/08/2026

--- tipedata.dart
+++ tipedata.dart
@@ -1,10 +1,53 @@
 void main() {
-  String nama = 'Vikho';
-  int umur = 20;
+  // String nama = 'Vikho';
+  // int umur = 20;
 
-  var nama1 = 'Vikho';
-  var angka1 = 100;
+  // var nama1 = 'Vikho';
+  // var angka1 = 100;
 
-  print('Halo! nama saya $nama. Umur saya $umur');
-  print('Halo! nama saya $nama. Umur saya $umur');
+  // print('Halo! nama saya $nama. Umur saya $umur');
+
+  // int angka1 = 100;
+  // double angka2 = 20.5;
+
+  // num hasil = 0;
+  // hasil = angka1 + angka2;
+  // print(hasil);
+
+  // dynamic angka1 = 100;
+  // dynamic angka2 = 200.5;
+  // dynamic hasil = 0;
+  // hasil = angka1 + angka2;
+  // print(hasil);
+
+  // List<dynamic> namamhs = ['Vikho', 'Keju', 'Louis', 100, true];
+  // List<String> nama = ['Vikho', 'Keju', 'Louis'];
+  // List<num> angka = [12, 12.5, 13.0, -1, -12.5];
+  // print(namamhs[1]);
+
+  // List namamhs = [];
+  // namamhs.add('Wilson1');
+  // namamhs.add('Wilson2');
+  // namamhs.add('Wilson3');
+  // namamhs.add('Wilson4');
+  // print(namamhs.reversed);
+  // print(namamhs.last);
+  // print(namamhs.removeAt(3));
+
+  // Set angka = {1, 2, 3, 4, 1, 2, 3, 4};
+  // print(angka.elementAt(2));
+
+  // Map datamhs = {'nama': 'Ferry', 'umur': 30, 'kelas': 'A'};
+  // print(datamhs['kelas']);
+
+  // List<Map> datamhs = [
+  //   {'nama': 'Anton', 'umur': 30, 'kelas': 'A'},
+  //   {'nama': 'Deka', 'umur': 20, 'kelas': 'P'},
+  //   {'nama': 'Ferry', 'umur': 25, 'kelas': 'Q'}
+  // ];
+  // print('halo nama saya ${datamhs[1]['nama']}, umur saya ${datamhs[1]['umur']} tahun, kelas ${datamhs[1]['kelas']}');
+
+  // final String nama = 'Vikho';
+
+  // const int angka = 100;
 }
